0d91bb89f3ee9ca6023cfbaef415127686c0caa3
SergeySeroff <harek20@mail.ru>
2022-11-02 10:39:06 +0300

    lesson02

---
+++ Lisson02/Program.cs
@@ -0,0 +1,13 @@
+﻿string str = "5";
+string str2 = "2";
+
+int a = Convert.ToInt32(str);
+int b = Convert.ToInt32(str2);
+
+Console.Write(a+b);
+
+string str3 = "1,9";
+
+double c = Convert.ToDouble(str3);
+
+Console.Write(c);
